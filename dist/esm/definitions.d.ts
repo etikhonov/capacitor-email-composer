0fe35bfed1d358edd09c7ba812a430d0fa08f9aa
etikhonov <evgeniy.tikhonov@gmail.com>
2021-12-17 09:47:07 +0200

    build

---
+++ dist/esm/definitions.d.ts
@@ -0,0 +1,62 @@
+export interface EmailComposerPlugin {
+    /**
+     * Checks if the User can send a Mail
+     * iOS: Check if the current Device is configured to send mail
+     * Android: Currently does nothing
+     *
+     * @since 1.0.0
+     */
+    hasAccount(): Promise<{
+        hasAccount: boolean;
+    }>;
+    /**
+     * Open the E-Mail Composer
+     *
+     * @param options optional Options to pre-fill the E-Mail
+     * @since 1.0.0
+     */
+    open(options?: OpenOptions): Promise<void>;
+}
+export interface OpenOptions {
+    /**
+     * email addresses for TO field
+     *
+     * @since 1.0.0
+     */
+    to?: string[];
+    /**
+     * email addresses for CC field
+     *
+     * @since 1.0.0
+     */
+    cc?: string[];
+    /**
+     * email addresses for BCC field
+     *
+     * @since 1.0.0
+     */
+    bcc?: string[];
+    /**
+     * subject of the email
+     *
+     * @since 1.0.0
+     */
+    subject?: string;
+    /**
+     * email body
+     *
+     * @since 1.0.0
+     */
+    body?: string;
+    /**
+     * indicates if the body is HTML or plain text (primarily iOS)
+     *
+     * @since 1.0.1
+     */
+    isHtml?: boolean;
+    /**
+     * attachments absolute path
+     *
+     */
+    attachments?: string | string[];
+}
